f1de7ff5ea348cb0c14bb9ee9dcf8da2bf790341
Jacob M. <241238814+dearcanada@users.noreply.github.com>
2025-11-28 22:21:47 +0800

    Add calc input data live update logic and operator input validation

--- calculator/src/js/app.js
+++ calculator/src/js/app.js
@@ -10,3 +10,44 @@ function operate(a, operator, b) {
 };
 
 console.log(operate(operandA, operator, operandB));
+
+const calculator = document.querySelector('#calc');
+const calcInput = document.querySelector('#calcInput');
+
+calculator.addEventListener('click', upldateCalcData);
+
+function upldateCalcData(event) {
+  const targetData = event.target.getAttribute('data-target');
+
+
+  if (targetData === '1') calcInput.value += 1;
+  if (targetData === '2') calcInput.value += 2;
+  if (targetData === '3') calcInput.value += 3;
+  if (targetData === '4') calcInput.value += 4;
+  if (targetData === '5') calcInput.value += 5;
+  if (targetData === '6') calcInput.value += 6;
+  if (targetData === '7') calcInput.value += 7;
+  if (targetData === '8') calcInput.value += 8;
+  if (targetData === '9') calcInput.value += 9;
+  if (targetData === '0') calcInput.value += 0;
+
+  if (targetData === 'clear') calcInput.value = '';
+  // if (targetData === '=') calcInput.value = '';
+
+  if (targetData === '+' && canAddOperator(calcInput.value, '+')) calcInput.value += '+';
+  if (targetData === '-' && canAddOperator(calcInput.value, '-')) calcInput.value += '-';
+  if (targetData === '*' && canAddOperator(calcInput.value, '*')) calcInput.value += '*';
+  if (targetData === '/' && canAddOperator(calcInput.value, '/')) calcInput.value += '/';
+};
+
+function canAddOperator(inputValue, operator) {
+  if (!inputValue) return false;
+
+  if ( !/[-+*\/]/.test(inputValue.at(-1)) ) {
+    return true;
+  } else if (inputValue.at(-1) !== operator) {
+    calcInput.value = inputValue.slice(0, -1).concat(operator);
+  } else {
+    return false;
+  }
+};
