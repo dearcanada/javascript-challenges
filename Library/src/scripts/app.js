d2dbeae8f0522796caea651ad8df6eee34b92ec0
Jacob M. <241238814+dearcanada@users.noreply.github.com>
2026-02-01 19:27:05 +0800

    Separate data and DOM render responsibilities

--- Library/src/scripts/app.js
+++ Library/src/scripts/app.js
@@ -8,79 +8,83 @@ function Book(title, author, pages) {
   this.pages = pages;
   this.readStatus = false;
   this.id = crypto.randomUUID();
+};
 
-  this.read = function() {
-    this.readStatus = true;
-  };
-  this.info = function() {
+Book.prototype = Object.create({
+  info() {
+    let infoString = `The ${this.title} by ${this.author}, ${this.pages} pages`;
+    
+    if (this.readStatus === false) {
+      return infoString.concat(', not read yet.');
+    } else {
+      return infoString.concat(', read yet.');
+    };
+  },
+  readInfo() {
     return this.readStatus === false ? 'Not read yet' : 'Read yet'; 
-    
-    // let infoString = `The ${this.title} by ${this.author}, ${this.pages} pages`;
-    
-    // if (this.readStatus === false) {
-    //   return infoString.concat(', not read yet.');
-    // } else {
-    //   return infoString.concat(', read yet.');
-    // };
-  };
-};
-
-const bookInstances = [];
-
-function createBookData(title, author, pages) {
-  bookInstances.push(new Book(title, author, pages));
-
-  addBookToDOM(bookInstances.at(-1));
-};
-
-createBookData('Abysmal JavaScript Book', 'DearsodaCan', 600);
-
-// HTML Books display
-const addButton = document.querySelector('#add-btn');
-const closeButton = document.querySelector('#close-btn');
-const submitButton = document.querySelector('#submit-btn');
-const removeButton = document.querySelector('#remove-btn');
-const dialog = document.querySelector('#dialog');
-
-
-addButton.addEventListener('click', () => dialog.showModal())
-closeButton.addEventListener('click', () => dialog.close())
-
-submitButton.addEventListener('click', event => {
-  event.preventDefault();
-
-  const titleInput = (document.querySelector('#title-id')).value;
-  const authorInput = (document.querySelector('#author-id')).value;
-  const pagesInput = (document.querySelector('#pages-id')).value;
-  
-  createBookData(titleInput, authorInput, pagesInput);
-});
-
-removeButton.addEventListener('click', event => {
-  event.target.closest('.book').remove();
+  },
+  read() {
+    this.readStatus = true;    
+  },
 })
 
+const myLibrary = [];
 
-function addBookToDOM(book) {
-  const newBook = document.createElement('article');
-  const catalog = document.querySelector('#catalog-id'); // An error occurs if outside of its function
-
-  newBook.classList.add('book');
-  newBook.innerHTML = 
-    `\n
-      <h1 class="book__title">${book.title}</h1>
-      <span class="book__author"><span>Author:</span> ${book.author}</span>
-      <span class="book__pages"><span>Pages:</span> ${book.pages}</span>
-      <span class="book__status"><span>Status:</span> ${book.info()}</span>
-      <span class="book__id"><span>UID:</span> ${book.id}</span>
-      <button type="button" class="btn btn--remove" id="remove-btn">Remove</button>
-    `;
-
-  console.log(newBook);
-  
-  catalog.appendChild(newBook);
+function createBookData(title, author, pages) {
+  myLibrary.push(new Book(title, author, pages));  
+  renderDOM(myLibrary.at(-1));
 };
 
-// 1/31/26 issue:
-// If a book is created it has multiple IDs (it might be fixed by selecting a class instead of an id);
-// "remove book" removes a book from DOM only, not from the array
+// HTML Event Handlers
+const dialog = document.querySelector('#dialog');
+const addButton = document.querySelector('#add-btn');
+const removeButtons = [];
+const catalog = document.querySelector('#catalog-id');
+
+dialog.addEventListener('click', event => {
+  const target = event.target;
+
+  // Modal window event delegation
+  if (target.getAttribute('id') === 'submit-btn') {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-id');
+    const author = document.querySelector('#author-id');
+    const pages = document.querySelector('#pages-id');
+    
+    createBookData(title.value, author.value, pages.value);
+    dialog.close();
+  };
+  if (target.getAttribute('id') === 'cancel-btn') {
+    dialog.close();
+  };
+});
+
+addButton.addEventListener('click', () => dialog.showModal());
+
+function pushCurrentRemoveButton(button) {
+  removeButtons.push(button);
+
+  button.addEventListener('click', event => event.target.closest('.book').remove())
+};
+
+// DOM Render
+function renderDOM(book) {
+  let bookContentWrapper = document.createElement('article');
+  bookContentWrapper.classList.add('book');
+  bookContentWrapper.setAttribute('data-uid', book.id);
+    
+  bookContentWrapper.innerHTML = `\n
+    <h1 class="book__title"><span>The</span> ${book.title}</h1>
+    <span class="book__author"><span>Author:</span> ${book.author}</span>
+    <span class="book__pages"><span>Pages:</span> ${book.pages}</span>
+    <span class="book__status"><span>Status:</span> ${book.readInfo()}</span>
+    <span class="book__id"><span>UID:</span> ${book.id}</span>
+    <button type="button" class="btn btn--remove" data-action-btn="remove-btn">Remove</button>
+  `;
+
+  catalog.appendChild(bookContentWrapper);
+
+  pushCurrentRemoveButton(bookContentWrapper.querySelector('[data-action-btn]'));
+};
+
