616638dbb136b67f7b4330e7b60b09c6bc59fa2a
Jacob M. <241238814+dearcanada@users.noreply.github.com>
2026-03-19 19:14:34 +0800

    Add API & shared data .js file

---
+++ TODO-list/src/scripts/shared/processor.js
@@ -0,0 +1,35 @@
+import { projects } from "./shared.js";
+import { log } from "./shared.js";
+export { pushGroup, removeGroup, renameGroup, pushNote, removeNote, getProjects };
+
+const pushGroup = (name, groupArray) => {
+  if (projects[name]) {
+    return log(`A group with "${name}" name already exists`);
+  };
+  
+  projects[name] = groupArray;
+};
+
+const removeGroup = (name) => {
+  delete projects[name];
+};
+
+const pushNote = (note, group) => {
+  if (projects[group].find(element => element.title === note.title)) {
+    return log(`A note with "${note.title}" title already exists`);
+  };
+  
+  projects[group].push(note);
+};
+
+const removeNote = (note, group) => {
+  const index = projects[group].findIndex(object => object.title === note);
+  projects[group].splice(index, 1);  
+};
+
+const renameGroup = (oldName, newName) => {
+  projects[oldName] = newName;
+};
+
+const getProjects = () => projects;
+
---
+++ TODO-list/src/scripts/shared/shared.js
@@ -0,0 +1,11 @@
+/* Console App Shared */ 
+class Project {};
+
+export const projects = new Project();
+
+export const log = (message) => {
+  console.log(`[${Date.now()}] Logger: [${message}].`);
+};
+
+/* DOM Shared */ 
+export const groupsList = document.querySelector('#groups-id');
